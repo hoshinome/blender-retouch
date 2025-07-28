cff3dbf30c335d61da4ea774aa461243390ea946
hoshinome <hoshinome@outlook.jp>
2025-07-28 16:30:12 +0900

    blender retouch v1.0.0

---
+++ addon/operator/__init__.py
@@ -0,0 +1,13 @@
+import bpy
+from . import add_nodes
+
+classes  =  []
+classes +=  add_nodes.classes
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
