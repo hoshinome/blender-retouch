b490e6191d1dce8ed8ed4434e6fbae164caa15b8
hoshinome <hoshinome2525@gmail.com>
2026-05-24 20:28:32 +0900

    fix: バージョンを1.3.0に変更

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author": "wawawa",
     "description": "retouch your images with blender",
     "blender": (5, 1, 0),
-    "version": (1, 2, 1),
+    "version": (1, 3, 0),
     "location": "",
     "warning": "",
     "category": "Generic",
