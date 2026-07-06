067665a72b079f385bb2cf8266748e553a3d95e3
hoshinome <hoshinome2525@gmail.com>
2026-07-06 15:24:56 +0900

    chore: bump version to 1.6.0

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author": "wawawa",
     "description": "retouch your images with blender",
     "blender": (5, 1, 0),
-    "version": (1, 5, 0),
+    "version": (1, 6, 0),
     "location": "",
     "warning": "",
     "category": "Generic",
